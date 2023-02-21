9ca08e93c4c71e82d9c83fb59c58f23ee60b9173
Rodrigo Quelhas <r.quelhas@papers.ch>
2023-02-21 15:20:00 +0000

    improve ExecutionResult usage

--- codec/src/main/kotlin/acurast/codec/type/marketplace/ExecutionResult.kt
+++ codec/src/main/kotlin/acurast/codec/type/marketplace/ExecutionResult.kt
@@ -6,12 +6,15 @@ import acurast.codec.type.ToU8a
 /**
  * This structure represents the result of a job execution.
  */
-public data class ExecutionResult(val kind: Kind, val payload: ByteArray): ToU8a {
+public class ExecutionResult private constructor(public val kind: Kind, public val payload: ByteArray): ToU8a {
+    public companion object {
+        public fun success(payload: ByteArray): ExecutionResult = ExecutionResult(Kind.Success, payload)
+        public fun failure(payload: ByteArray): ExecutionResult = ExecutionResult(Kind.Failure, payload)
+    }
 
-    public enum class Kind(public val id: Byte): ToU8a {
+    public enum class Kind(private val id: Byte): ToU8a {
         Success(0),
         Failure(1);
-
         override fun toU8a(): ByteArray = id.toU8a()
     }
 
--- codec/src/test/kotlin/acurast/codec/extrinsic/MarketplaceTest.kt
+++ codec/src/test/kotlin/acurast/codec/extrinsic/MarketplaceTest.kt
@@ -68,7 +68,7 @@ class MarketplaceTest {
             requester = AccountId32("8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48".hexToBa()),
             script = script,
         )
-        val executionResult = ExecutionResult(ExecutionResult.Kind.Success, """{ "some_field": 1 }""".encodeToByteArray())
+        val executionResult = ExecutionResult.success("""{ "some_field": 1 }""".encodeToByteArray())
         val call = ReportCall(callIndex, jobId, false, executionResult)
         val expected = "2b048eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48d4697066733a2f2f516d5378377a44706b76627975674c33553339467454617357784d6d6b664736377378397761475256483741514500004c7b2022736f6d655f6669656c64223a2031207d"
         Assert.assertEquals(expected, call.toU8a().toHex())
