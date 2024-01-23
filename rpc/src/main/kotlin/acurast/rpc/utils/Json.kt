51fdaae156e4510c172ca302f386f0d9b2f6d618
Julia Samol <j.samol@papers.ch>
2024-01-23 10:05:22 +0100

    chore(rpc): fix comment wording

--- rpc/src/main/kotlin/acurast/rpc/utils/Json.kt
+++ rpc/src/main/kotlin/acurast/rpc/utils/Json.kt
@@ -9,7 +9,7 @@ internal const val JSON_RPC_KEY_RESULT = "result"
  * - if the key is not present in the object, the method returns an empty string `""`
  * - if the key is present but has no value, i.e. the value is `null`, the method returns the string `"null"`
  *
- * This behavior is counterintuitive and can bring various unexpected effects.
+ * This behavior is counterintuitive and can lead to various unexpected effects.
  * `JSONObject.nullableOptString` tries to mitigate the issue by checking whether the value actually exists before
  * trying to access and return it, and returning `null` if it doesn't.
  */
