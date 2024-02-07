efe4d2b3a79d5bed0a6e1c6eab3688927ca24b83
Julia Samol <j.samol@papers.ch>
2024-02-07 14:16:24 +0100

    Feat/rpc-error-data

--- rpc/src/main/kotlin/acurast/rpc/pallet/PalletRpc.kt
+++ rpc/src/main/kotlin/acurast/rpc/pallet/PalletRpc.kt
@@ -3,6 +3,7 @@ package acurast.rpc.pallet
 import acurast.rpc.JsonRpc
 import acurast.rpc.Rpc
 import acurast.rpc.engine.RpcEngine
+import acurast.rpc.utils.nullableOptString
 import com.google.gson.GsonBuilder
 import org.json.JSONArray
 import org.json.JSONObject
@@ -14,7 +15,11 @@ public abstract class PalletRpc(override val defaultEngine: RpcEngine) : Rpc {
 
     protected fun handleError(json: JSONObject): Exception =
         json.optJSONObject(JsonRpc.Key.ERROR)?.let { error ->
-            Exception(error.optString(JsonRpc.Key.MESSAGE))
+            val message = error.optString(JsonRpc.Key.MESSAGE)
+            val data = error.nullableOptString(JsonRpc.Key.DATA)
+            val exceptionMessage = data?.let { "$message ($it)" } ?: message
+
+            Exception(exceptionMessage)
         } ?: Exception("something went wrong")
 
     protected inline fun <reified R> parseJSON(json: String): R =
