96be7b621e465cf0e23a8aa106c9ec51eb243b09
Simon Wehrli <simon@smartnuance.com>
2024-07-31 13:37:59 +0100

    feat: add MutliOrigin variants

--- codec/src/main/kotlin/acurast/codec/type/acurast/JobIdentifier.kt
+++ codec/src/main/kotlin/acurast/codec/type/acurast/JobIdentifier.kt
@@ -18,24 +18,33 @@ public data class MultiOrigin(
     public companion object {
         public fun Acurast(address: AccountId32): MultiOrigin = MultiOrigin(Kind.Acurast, address.toU8a())
         public fun Tezos(payload: ByteArray): MultiOrigin = MultiOrigin(Kind.Tezos, payload.toU8a())
+        public fun AlephZero(address: AccountId32): MultiOrigin = MultiOrigin(Kind.AlephZero, address.toU8a())
+        public fun Vara(address: AccountId32): MultiOrigin = MultiOrigin(Kind.Vara, address.toU8a())
 
         public fun read(buffer: ByteBuffer): MultiOrigin {
             return when (val kind = buffer.readByte()) {
                 Kind.Acurast.id -> Acurast(buffer.readAccountId32())
                 Kind.Tezos.id -> Tezos(buffer.readByteArray())
+                Kind.AlephZero.id -> AlephZero(buffer.readAccountId32())
+                Kind.Vara.id -> Vara(buffer.readAccountId32())
                 else -> throw UnsupportedEncodingException("Unknown multi origin kind $kind.")
             }
         }
     }
     public enum class Kind(public val id: Byte): ToU8a {
         Acurast(0),
-        Tezos(1);
+        Tezos(1),
+        // Ethereum(2),
+        AlephZero(3),
+        Vara(4);
 
         public override fun toU8a(): ByteArray = id.toU8a()
 
         public override fun toString(): String = when (this) {
             Acurast -> "Acurast"
             Tezos -> "Tezos"
+            AlephZero -> "AlephZero"
+            Vara -> "Vara"
         }
     }
 
