d607b37d68c45c8a3a0c052d64fa8b766283ed42
Julia Samól <j.samol@papers.ch>
2025-02-24 14:18:16 +0100

    feat(codec): update Metrics struct

--- codec/src/main/kotlin/acurast/codec/type/manager/Metrics.kt
+++ codec/src/main/kotlin/acurast/codec/type/manager/Metrics.kt
@@ -1,31 +1,32 @@
 package acurast.codec.type.manager
 
+import acurast.codec.extensions.toU8a
 import acurast.codec.type.ToU8a
 import acurast.codec.type.UInt128
 import java.math.BigDecimal
 import java.math.BigInteger
 
 public data class Metrics(
-    val poolName: String, // 8 bytes
+    val poolId: Byte,
     val numerator: UInt128,
     val denominator: UInt128,
 ) : ToU8a {
     override fun toU8a(): ByteArray =
-        poolName.toByteArray(charset = Charsets.UTF_8).copyOf(8) + numerator.toU8a() + denominator.toU8a()
+        poolId.toU8a() + numerator.toU8a() + denominator.toU8a()
 
     public companion object
 }
 
-public fun Metrics(poolName: String, numerator: BigInteger, denominator: BigInteger): Metrics =
-    Metrics(poolName, UInt128(numerator), UInt128(denominator))
+public fun Metrics(poolId: Byte, numerator: BigInteger, denominator: BigInteger): Metrics =
+    Metrics(poolId, UInt128(numerator), UInt128(denominator))
 
-public fun Metrics(poolName: String, value: Long): Metrics =
-    Metrics(poolName, BigInteger.valueOf(value), BigInteger.ONE)
+public fun Metrics(poolId: Byte, value: Long): Metrics =
+    Metrics(poolId, BigInteger.valueOf(value), BigInteger.ONE)
 
-public fun Metrics(poolName: String, value: Double): Metrics {
+public fun Metrics(poolId: Byte, value: Double): Metrics {
     val value = BigDecimal(value.toString())
     val numerator = value.unscaledValue()
     val denominator = BigInteger.TEN.pow(value.scale())
 
-    return Metrics(poolName, numerator, denominator)
+    return Metrics(poolId, numerator, denominator)
 }
